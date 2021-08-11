cce0e710a8a5c81f0a77ea4ec2523301d996a20a
MargoZacharska <malgorzata-zacharska@wp.pl>
2021-08-11 19:38:09 +0200

    Add basic activity

--- CookMaster/app/src/main/java/com/example/cookmaster/MainActivity.java
+++ CookMaster/app/src/main/java/com/example/cookmaster/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.cookmaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_search:
+                        Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_recent:
+                        Toast.makeText(MainActivity.this, "Recents", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_favorites:
+                        Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.my_week:
+                        Toast.makeText(MainActivity.this, "My week", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_shopping:
+                        Toast.makeText(MainActivity.this, "Shopping list", Toast.LENGTH_SHORT).show();
+                        setContentView(R.layout.shopping_list);
+                        break;
+                }
+                return true;
+            }
+        });
     }
+
 }
