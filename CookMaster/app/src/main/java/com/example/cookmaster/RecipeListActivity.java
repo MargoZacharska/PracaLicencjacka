7906f7362b4a30e2632e36ed13959799e1ab245a
MargoZacharska <malgorzata-zacharska@wp.pl>
2021-08-31 18:42:17 +0200

    Allow to filter recipes

--- CookMaster/app/src/main/java/com/example/cookmaster/RecipeListActivity.java
+++ CookMaster/app/src/main/java/com/example/cookmaster/RecipeListActivity.java
@@ -2,6 +2,10 @@ package com.example.cookmaster;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,7 +20,26 @@ public class RecipeListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
-        
-        ((ListView)findViewById(R.id.recipe_list_view)).setAdapter(new RecipeListAdapter(this, dataService.GetRecipes(this)));
+
+        RecipeListAdapter adapter = new RecipeListAdapter(this, dataService.GetRecipes(this));
+        ListView recipeList = ((ListView)findViewById(R.id.recipe_list_view));
+        recipeList.setAdapter(adapter);
+        recipeList.setTextFilterEnabled(true);
+
+        ((EditText)findViewById(R.id.recipe_search)).addTextChangedListener(new TextWatcher(){
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                adapter.getFilter().filter(s);
+            }
+        });
     }
 }
--- CookMaster/app/src/main/java/com/example/cookmaster/services/RecipeListAdapter.java
+++ CookMaster/app/src/main/java/com/example/cookmaster/services/RecipeListAdapter.java
@@ -4,22 +4,27 @@ import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.cookmaster.R;
 import com.example.cookmaster.model.Recipe;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class RecipeListAdapter extends BaseAdapter {
+public class RecipeListAdapter extends BaseAdapter implements Filterable {
 
     private Activity parent;
+    private List<Recipe> allRecipes;
     private List<Recipe> recipes;
 
     public RecipeListAdapter(Activity activity, List<Recipe> recipes) {
         this.parent = activity;
         this.recipes = recipes;
+        this.allRecipes = recipes;
     }
     @Override
     public int getCount() {
@@ -49,5 +54,38 @@ public class RecipeListAdapter extends BaseAdapter {
         ((TextView) convertView.findViewById(R.id.recipe_preparation_time)).setText("Czas przygotowania: " + recipe.preparationTime);
 
         return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint == null || constraint.length() == 0) {
+                    filterResults.count = allRecipes.size();
+                    filterResults.values = allRecipes;
+
+                } else {
+                    List<Recipe> resultsModel = new ArrayList<>();
+                    String searchStr = constraint.toString().toLowerCase();
+
+                    for (Recipe recipe : allRecipes) {
+                        if (recipe.title.contains(searchStr) || recipe.category.contains(searchStr)) {
+                            resultsModel.add(recipe);
+                        }
+                        filterResults.count = resultsModel.size();
+                        filterResults.values = resultsModel;
+                    }
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                recipes = (List<Recipe>)results.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
